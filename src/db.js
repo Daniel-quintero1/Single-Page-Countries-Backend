93292552e8f9734c0930f93680ed5176d80d312a
Daniel Quintero <daqcarrillo@gmail.com>
2023-10-02 19:05:22 -0300

    modificacion del Db.js con comentarios

--- src/db.js
+++ src/db.js
@@ -4,11 +4,27 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/country`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/country`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: DB_HOST,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: 'country', // El nombre de tu base de datos
+  logging: false,
 });
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
 // const sequelize = new Sequelize(
 //   DB_DEPLOY,
 //   {
